0faf75b01bf831d28328343fddfea7fcc27d43c2
Colin Tinsman <tinsmancb@gmail.com>
2023-11-09 15:15:35 -0500

    implement append and str

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 class Node:
-    pass
+    def __init__(self, val=None):
+        if val is not None:
+            self.value = val
+            self.next = Node()
+        else:
+            self.value = None
+            self.next = None
+
+    def append(self, val):
+        if self.value is not None:
+            self.next.append(val)
+        else:
+            self.value = val
+            self.next = Node()
+
+    def __str__(self):
+        if self.value is not None:
+            out = str(self.value)
+            if self.next.value is not None:
+                out += ','
+                out += str(self.next)
+            return out
+        else:
+            return ''
 
 
 def main():
-    pass
+    mylist = Node()
+    mylist.append(1)
+    mylist.append(2)
+    mylist.append(3)
+    print(mylist)
 
 
 if __name__ == '__main__':
